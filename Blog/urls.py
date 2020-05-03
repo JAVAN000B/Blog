9b01bfe495717b8191e25a36672c378229896742
minghui <smh335789877@gmail.com>
2020-05-03 22:07:19 +0800

    finish half of Blog front end

--- Blog/urls.py
+++ Blog/urls.py
@@ -21,7 +21,12 @@ from myBlog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='index'),  # 网站首页
+    path('list-<int:lid>.html', views.list, name='list'),  # 列表页
+    path('show-<int:sid>.html', views.show, name='show'),  # 内容页
+    path('tag/<tag>', views.tag, name='tags'),  # 标签列表页
+    path('s/', views.search, name='search'),  # 搜索列表页
+    path('about/', views.about, name='about'),  # 联系我们单页
     path('ueditor/', include('DjangoUeditor.urls')),
     re_path('^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
