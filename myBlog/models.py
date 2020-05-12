3569d4ad74ba966a0fa5e935ea48ab79d966459f
Simon <39431803+JAVAN000B@users.noreply.github.com>
2020-05-12 18:02:24 +0800

    delete useless plugin

--- myBlog/models.py
+++ myBlog/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from ckeditor.fields import RichTextField
-from DjangoUeditor.models import UEditorField
 
 
 # Create your models here.
